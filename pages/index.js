9148394eecdd44594a92c7f6b035d7f2079a41e8
Michael Cillo <mjc.rrnewtech@gmail.com>
2022-12-06 15:00:58 -0500

    added test photo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import newLogo from '../public/Logo2.png'
+import newLogo from '../public/Tucker.jpg'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
