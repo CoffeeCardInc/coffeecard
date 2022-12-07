f574472f277b2bf500bf8d6b109b0d08089d6a11
Michael Cillo <mjc.rrnewtech@gmail.com>
2022-12-07 15:00:01 -0500

    updated logo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import newLogo from '../public/Tucker.jpg'
+import newLogo from '../public/cc_logo.png'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
